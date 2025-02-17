a5b3dff3cfa8e8c7890891d04bf0654600789098
sugisaki <sugisaki.katsuyuki@gmail.com>
2025-02-17 17:35:09 +0900

    プライバシーポリシーをシンフリーサーバにアップロードするスクリプト

--- README.md
+++ README.md
@@ -3,12 +3,21 @@
 ## プライバシーポリシー
 ./android には静的ページ、プライバシーポリシー。
 roundrun は日本語ページと英語ページがあるので注意。
+### シンフリーサーバーにプライバシーポリシーをデプロイ
+静的ページはこのスクリプトでコピーする。
+upload_privacy_to_shin-server.sh
 
-## React 実験
+
+## React チュートリアルページ
 React ビルドの生成物は react-project/dist
+### Vercelに Reactデプロイ
+React はワークフロー使わずにデプロイ。
+Vercel ではワークフローで Reactと静的ページを混ぜる処理できないので、
+静的ページは別途。
 
-## デプロイ
-###  GitHubワークフロー
+
+## GitHubワークフローで GitHub Pagesにデプロイ
 .github/workflows/deploy.yml
 Reactページと静的ページ、両方デプロイしたいので、
 ./deployment ディレクトリに両方をコピーしてアップロードする。
+
---
+++ upload_privacy_to_shin-server.sh
@@ -0,0 +1,7 @@
+# シンフリーサーバーにプライバシーポリシーをアップロードする
+scp android/roundrun/privacy.en.html shin-server:~/studio-kei.info/public_html/android/roundrun/.
+scp android/roundrun/privacy.ja.html shin-server:~/studio-kei.info/public_html/android/roundrun/.
+#scp android/roundrun/privacy.html shin-server:~/studio-kei.info/public_html/android/roundrun/.
+scp android/seasoncalendar/privacy.html shin-server:~/studio-kei.info/public_html/android/seasoncalendar/.
+scp android/tianmingcalendar/privacy.html shin-server:~/studio-kei.info/public_html/android/tianmingcalendar/.
+
